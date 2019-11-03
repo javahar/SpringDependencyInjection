c26299eb218b119ff4a7398024189fc2ed4442c0
jawaharjasthi <jawahar.jasthi@yahoo.com>
2019-11-03 12:50:23 -0700

    first basic commit

---
+++ greetings/src/test/java/com/greeting/service/greetings/GreetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greeting.service.greetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
